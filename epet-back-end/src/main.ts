60d5e4c76498bd62725bdc1e8c2b3c917fd358d0
StyxK <nosomething050@gmail.com>
2019-03-19 09:30:45 +0700

    [ADD]  dotenv

--- epet-back-end/src/main.ts
+++ epet-back-end/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config'
+
+const PORT = process.env.PORT
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.log(`http:\\localhost:${PORT} is running`)
 }
 bootstrap();
