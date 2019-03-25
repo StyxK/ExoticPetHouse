5b7cdf4648dce06e8080e9d8cf7081f42e3e425e
StyxK <nosomething050@gmail.com>
2019-03-25 23:14:05 +0700

    [ADD] react-native-maps

--- epet_frontend_customer/views/Search.js
+++ epet_frontend_customer/views/Search.js
@@ -9,7 +9,8 @@
 
 import React, {Component} from 'react';
 import {Container,Content} from 'native-base';
-import NavHeader from '../components/NavHeader.js';
+import {View,StyleSheet} from 'react-native';
+import MapView from 'react-native-maps'
 import NavFooter from '../components/NavFooter.js';
 
 // type Props = {};
@@ -17,9 +18,30 @@ export default class Search extends Component {
   render() {
     return (
       <Container>
-        <Content/>
-        <NavFooter/>
+        <View style={styles.container}>
+          <MapView style={styles.map}
+              initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+            />
+        </View>
+      <NavFooter/>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    marginTop: 1.5,
+    ...StyleSheet.absoluteFillObject,
+  },
+})
