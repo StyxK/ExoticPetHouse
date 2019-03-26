9c6a935eb8d91c30e743106be1230842cbe75374
StyxK <nosomething050@gmail.com>
2019-03-26 17:26:55 +0700

    [EDIT] stores marker
    mark ที่อยู่ร้านด้วย latitude & longitude ด้วยการ loop

--- epet_frontend_customer/components/NavFooter.js
+++ epet_frontend_customer/components/NavFooter.js
@@ -7,24 +7,24 @@ export default class NavFooter extends Component{
             <Footer>
                 <FooterTab style={{backgroundColor:'#7A5032'}}>
                     <Button Badge vertical>
-                        <Icon name='search'/>
-                        <Text style={{fontSize:10}}>search</Text>
+                        <Icon name='search' style={{color:'white'}}/>
+                        <Text style={{fontSize:10,color:'white'}}>search</Text>
                     </Button>
                     <Button Badge vertical>
-                        <Icon name='paw'/>
-                        <Text style={{fontSize:10}}>my pets</Text>
+                        <Icon name='paw' style={{color:'white'}}/>
+                        <Text style={{fontSize:10,color:'white'}}>my pets</Text>
                     </Button>
                     <Button Badge vertical>
-                        <Icon name='chatbubbles'/>
-                        <Text style={{fontSize:10}}>chat</Text>
+                        <Icon name='chatbubbles' style={{color:'white'}}/>
+                        <Text style={{fontSize:10,color:'white'}}>chat</Text>
                     </Button>
                     <Button Badge vertical>
-                        <Icon name='list'/>
-                        <Text style={{fontSize:10}}>history</Text>
+                        <Icon name='list' style={{color:'white'}}/>
+                        <Text style={{fontSize:10,color:'white'}}>history</Text>
                     </Button>
                     <Button Badge vertical>
-                        <Icon name='person'/>
-                        <Text style={{fontSize:10}}>profile</Text>
+                        <Icon name='person' style={{color:'white'}}/>
+                        <Text style={{fontSize:10,color:'white'}}>profile</Text>
                     </Button>
                 </FooterTab>
             </Footer>    
--- epet_frontend_customer/views/Search.js
+++ epet_frontend_customer/views/Search.js
@@ -9,24 +9,67 @@
 
 import React, {Component} from 'react';
 import {Container,Content} from 'native-base';
-import {View,StyleSheet} from 'react-native';
-import MapView from 'react-native-maps'
+import {View,StyleSheet,Text} from 'react-native';
+import MapView,{Marker} from 'react-native-maps'
 import NavFooter from '../components/NavFooter.js';
 
 // type Props = {};
+
+const store = [
+  {
+    name:'ดำรงค์สัตว์แพทย์',
+    latitude:10.5774781,
+    longitude:101.44130759,
+  },
+  {
+    name:'นุนิสัตว์แปลกน่ารัก',
+    latitude:12.5774781,
+    longitude:105.44130759,
+  },
+  {
+    name:'DogePetHouse',
+    latitude:15.5774781,
+    longitude:102.44130759,
+  }
+]
+
 export default class Search extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      initialPoint:{
+        latitude:13.5774781,
+        longitude:100.44130759,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      startPoint:{
+        latitude:13.5774781,
+        longitude:100.44130759
+      }
+    }
+  }
+
   render() {
+
+    let storeMarker =[];
+    for(i in store){
+      storeMarker.push(
+        <Marker key={i} coordinate={{
+          latitude:store[i].latitude,
+          longitude:store[i].longitude
+        }} pinColor={'#7A5032'} title={store[i].name} />
+      )
+    }
+
     return (
       <Container>
         <View style={styles.container}>
-          <MapView style={styles.map}
-              initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-            />
+          <MapView style={styles.map} initialRegion={this.state.initialPoint}>
+            <Marker coordinate={this.state.startPoint}/>
+            {storeMarker}
+          </MapView>
         </View>
       <NavFooter/>
       </Container>
