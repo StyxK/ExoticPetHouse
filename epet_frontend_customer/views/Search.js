f44da6825dda3c3054c883149f60a928fc2a6227
Vuttichai <vuttichai.lor@mail.kmutt.ac.th>
2019-03-31 17:35:05 +0700

    [EDIT & ADD] edit layout and add search box

--- epet_frontend_customer/views/Search.js
+++ epet_frontend_customer/views/Search.js
@@ -9,7 +9,7 @@
 
 import React, { Component } from 'react';
 import { Container, Content, Button, Icon, ListItem, List, Text, Left, Body, Right } from 'native-base';
-import { View, StyleSheet, Modal, Alert, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, Modal, Alert, TouchableHighlight, TextInput } from 'react-native';
 import MapView, { Marker } from 'react-native-maps'
 import NavFooter from '../components/NavFooter.js';
 import axios from "axios";
@@ -52,7 +52,7 @@ export default class Search extends Component {
   }
 
   render() {
-    const { stores, startPoint, initialPoint, ModalVisible} = this.state;
+    const { stores, startPoint, initialPoint, ModalVisible } = this.state;
 
     let storeMarker = [];
     let storeList = [];
@@ -76,9 +76,9 @@ export default class Search extends Component {
             <Text note>{store.descripion}</Text>
           </Body>
           <Right>
-            {(startPoint && store.address) ? 
-            <Text note>distance {distance(startPoint.latitude,startPoint.longitude,store.address.latitude,store.address.longitude,"K").toFixed(2)} km.</Text>
-            : <Text note>N/A</Text>
+            {(startPoint && store.address) ?
+              <Text note>distance {distance(startPoint.latitude, startPoint.longitude, store.address.latitude, store.address.longitude, "K").toFixed(2)} km.</Text>
+              : <Text note>N/A</Text>
             }
           </Right>
         </ListItem>
@@ -87,59 +87,69 @@ export default class Search extends Component {
 
     return (
       <Container>
-        <View style={styles.container}>
+        <View style={styles.map}>
           <MapView style={styles.map} initialRegion={initialPoint}>
             <Marker coordinate={startPoint} />
             {storeMarker}
           </MapView>
-            <View style={{alignSelf:"flex-end"}}>
-              <Button style={{borderRadius:20,height:35}} 
-                  onPress={()=>{this.setModalVisible(true)}}
-                  visible={!ModalVisible}>
-                <Text>Show Store List</Text>
-              </Button>
-            </View>
-          <Modal animationType="slide" transparent={true} visible={ModalVisible} 
-            onRequestClose={()=>{
+        </View>
+        <View style={styles.container}>
+          <TextInput
+            style={{ height: 40, width: "90%", borderColor: 'gray',backgroundColor: "white", borderRadius: 10, margin:20, borderWidth: 1 }}
+            editable={true}
+            placeholder="Search"
+            onChangeText={this.onSearchTextChange}
+          />
+          <Button style={{ borderRadius: 20, height: 35, alignSelf: "center" }}
+            onPress={() => { this.setModalVisible(true) }}
+            visible={!ModalVisible}>
+            <Text>Show Store List</Text>
+          </Button>
+          <Modal animationType="slide" transparent={true} visible={ModalVisible}
+            onRequestClose={() => {
               Alert.alert('Modal Closed')
-          }}>
+            }}>
             <View style={styles.modalContainer}>
               <Content style={styles.modal}>
-                <TouchableHighlight onPress={()=>{this.setModalVisible(!ModalVisible)}} style={{alignItems:'center'}}>
-                  <Text style={{marginTop:7}}>Hide Modal</Text>
+                <TouchableHighlight onPress={() => { this.setModalVisible(!ModalVisible) }} style={{ alignItems: 'center' }}>
+                  <Text style={{ marginTop: 7 }}>Hide Modal</Text>
                 </TouchableHighlight>
                 <List>{storeList}</List>
               </Content>
             </View>
           </Modal>
         </View>
-        
-      <NavFooter/>
+
+        <NavFooter />
       </Container>
     );
+  }
+
+  onSearchTextChange = (text) => {
+    console.log(text)
   }
 }
 
 
 function distance(lat1, lon1, lat2, lon2, unit) {
-	if ((lat1 == lat2) && (lon1 == lon2)) {
-		return 0;
-	}
-	else {
-		var radlat1 = Math.PI * lat1/180;
-		var radlat2 = Math.PI * lat2/180;
-		var theta = lon1-lon2;
-		var radtheta = Math.PI * theta/180;
-		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		if (dist > 1) {
-			dist = 1;
-		}
-		dist = Math.acos(dist);
-		dist = dist * 180/Math.PI;
-		dist = dist * 60 * 1.1515;
-		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
-		return dist;
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+    return 0;
+  }
+  else {
+    var radlat1 = Math.PI * lat1 / 180;
+    var radlat2 = Math.PI * lat2 / 180;
+    var theta = lon1 - lon2;
+    var radtheta = Math.PI * theta / 180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") { dist = dist * 1.609344 }
+    if (unit == "N") { dist = dist * 0.8684 }
+    return dist;
   }
 }
 
@@ -148,7 +158,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: "row"
+    flexDirection: "column",
+    justifyContent: "space-between"
   },
   map: {
     marginTop: 1.5,
@@ -157,6 +168,7 @@ const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
     flexDirection: 'column-reverse',
+    justifyContent: 'center',
     alignItems: 'center',
     height: 500
   },
