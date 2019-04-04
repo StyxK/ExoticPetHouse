fe1aeba76e87997c06952ab16460e3e6cfb58c3e
Vuttichai <vuttichai.lor@mail.kmutt.ac.th>
2019-04-04 21:18:55 +0700

    [FIX]app.module

--- epet-back-end/src/app.module.ts
+++ epet-back-end/src/app.module.ts
@@ -11,12 +11,11 @@ import { EmployeeModule } from './employee/employee.module';
 import { CustomerModule } from './customer/customer.module';
 import { PetService } from './pet/pet.service';
 import { PetController } from './pet/pet.controller';
-import { MoController } from './pet/mo/mo.controller';
 import { PetModule } from './pet/pet.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(),StoreModule,AddressModule, CageModule, OrderModule,StoreownerModule, EmployeeModule, CustomerModule, PetModule],
-  controllers: [AppController, PetController, MoController,],
+  controllers: [AppController, PetController,],
   providers: [AppService, PetService,],
 })
 export class AppModule {}
