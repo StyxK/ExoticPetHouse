c46d4224c5908c5bcbb01a63a1f27184938f0fad
Vuttichai <vuttichai.lor@mail.kmutt.ac.th>
2019-04-07 19:27:06 +0700

    [EDIT] feedback module

--- epet-back-end/src/feedback/feedback.module.ts
+++ epet-back-end/src/feedback/feedback.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from 'src/customer/customer.entity';
+import { Order } from 'src/order/order.entity';
 import { FeedbackController } from './feedback.controller';
+import { Feedback } from './feedback.entity';
 import { FeedbackService } from './feedback.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feedback, Order, Customer])],
+  exports: [FeedbackService],
   controllers: [FeedbackController],
   providers: [FeedbackService]
+
 })
-export class FeedbackModule {}
+export class FeedbackModule { }
