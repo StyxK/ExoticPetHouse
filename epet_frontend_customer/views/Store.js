e9173b75f68f96d48cbe60b73a4ab16321bc7ff1
FlookSupakorn <supakorn.flook@mail.kmutt.ac.th>
2019-04-07 18:26:24 +0700

    [EDIT] Store for show cage and choose cage

--- epet_frontend_customer/views/Store.js
+++ epet_frontend_customer/views/Store.js
@@ -1,4 +1,4 @@
-import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { Container, Content, Card, CardItem, Text, Button, Icon, Left, Body, Right, ListItem, List, Radio} from 'native-base';
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import axios from 'axios';
@@ -13,6 +13,8 @@ export default class Store extends Component {
         this.state = {
           stores: [],
           address:{},
+          cage:[],
+          cageSelected: "cageId"
         }
     }
 
@@ -26,8 +28,9 @@ export default class Store extends Component {
           .get(API_URL + '/store/'+this.storeId)
           .then(response => {
             this.setState({
-              stores: response.data,
-              address:JSON.parse(JSON.stringify(response.data.address))
+                stores: response.data,
+                address:JSON.parse(JSON.stringify(response.data.address)),
+                cage:JSON.parse(JSON.stringify(response.data.cage))
             })
             console.log(JSON.stringify(response))
           }).then(error => console.log(error))
@@ -35,8 +38,31 @@ export default class Store extends Component {
             .get(API_URL+ '/')
     }
 
+    
+    
     render() {
-        const {stores,address} = this.state;
+        let storeList = this.state.cage.map((data)=>{
+            return  <ListItem avatar key={data.id} onPress={() => this.setState({ cageSelected:data.id })}>
+                      <Left>
+                      <Icon name='paw' />
+                      </Left>
+                      <Body>
+                        <Text>{data.name}</Text>
+                        <Text note>ประเภท: {data.type}</Text>
+                        <Text style={{color:'#7A5032'}}>{data.price} บาท/คืน</Text>
+                      </Body>
+                      <Right>
+                        <Radio
+                                color={"#f0ad4e"}
+                                selectedColor={"#5cb85c"}
+                                selected={this.state.cageSelected == data.id}
+                                
+                            />
+                      </Right>
+                    </ListItem>
+        });
+
+        const {stores,address,cage} = this.state;
         return (
             <View style={styles.container}>          
                 <Container>
@@ -76,8 +102,14 @@ export default class Store extends Component {
                                     <Text>Total Cage</Text>
                                     <Text note style={{color:'#7A5032'}}>{stores.maxOfDeposit}</Text>
                                 </Left>
-                            </CardItem>
+                            </CardItem> 
+                            <List>{storeList}</List>
+                            
+                            
                         </Card>
+                        
+                        <Text>{this.state.cageSelected}</Text>
+                        
                     </Content>
                 </Container>
             </View>
