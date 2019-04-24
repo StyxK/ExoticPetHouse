ed13e45f855c2b2b636cb38bc405a0fd8acfacb5
Vuttichai <vuttichai.lor@mail.kmutt.ac.th>
2019-04-24 10:28:20 +0700

    [FIX] show pet in my pet

--- epet_frontend_customer/views/Main.js
+++ epet_frontend_customer/views/Main.js
@@ -2,44 +2,36 @@ import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Router, Scene, Actions } from "react-native-router-flux";
 import NavFooter from "../components/NavFooter.js";
-import { Provider } from 'react-redux';
 import MyPet from "./MyPet";
 import Search from "./Search";
 import Store from "./Store";
 import AddPet from "./AddPet.js";
 import PetDescription from "./PetDescription.js";
 import Order from "./Order";
-import { createStore } from 'redux';
-import petsReducer from "../reducers/petsReducer.js"
+import { createStore } from "redux";
+import petsReducer from "../reducers/petsReducer.js";
 
-const store = createStore(petsReducer)
+const store = createStore(petsReducer);
 
 export default class Main extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Router>
-            <Scene key="root" hideNavBar={true}>
-              <Scene
-                key="home"
-                component={Search}
-                title="Home"
-                initial={true}
-              />
-              <Scene key="myPet" component={MyPet} title="MyPet" />
-              <Scene key="store" component={Store} title="Store" />
-              <Scene key="addPet" component={AddPet} title="AddPet" />
-              <Scene
-                key="petDescription"
-                component={PetDescription}
-                title="PetDescription"
-              />
-              <Scene key="order" component={Order} title="Order" />
-            </Scene>
-          </Router>
-        </View>
-      </Provider>
+      <View style={styles.container}>
+        <Router>
+          <Scene key="root" hideNavBar={true}>
+            <Scene key="home" component={Search} title="Home" initial={true} />
+            <Scene key="myPet" component={MyPet} title="MyPet" />
+            <Scene key="store" component={Store} title="Store" />
+            <Scene key="addPet" component={AddPet} title="AddPet" />
+            <Scene
+              key="petDescription"
+              component={PetDescription}
+              title="PetDescription"
+            />
+            <Scene key="order" component={Order} title="Order" />
+          </Scene>
+        </Router>
+      </View>
     );
   }
 }
