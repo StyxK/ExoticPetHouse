46fac053e1ccba8440601a00bbefbdea4dca9e28
StyxK <nosomething050@gmail.com>
2019-06-14 01:16:57 +0700

    [EDIT] validate all field in create store form
    - ทำ validate data ถ้ายังกรอกผิดพลาด หรือไม่กรอก จะ submit ข้อมูลไม่ได้้
    - สามารถส่งไปให้ api ได้
    - ยังเคลียร์ฟอร์มหลังจากกรอกเสร็จไม่ได้

--- epet_frontend_provider/views/CreateStore.js
+++ epet_frontend_provider/views/CreateStore.js
@@ -1,173 +1,98 @@
 import React,{Component} from 'react'
 import { Container, Text , Header , Left , Right , Body, Form , Item, Label, Textarea, ListItem, View, Button, Input, Icon, FooterTab, Content, Footer} from 'native-base'
-import Config from 'react-native-config'
-import { Field,reduxForm} from 'redux-form'
+import { Field,reduxForm,reset} from 'redux-form'
 import { Actions } from 'react-native-router-flux'
+import {DispatchProp} from 'react-redux'
 import axios from 'axios'
 
 class CreateStore extends Component{
 
     constructor(props){
         super(props)
-        this.state = {
-            storeRegistionDetail : {
-                rating : 0
-            },
-            address : {}
-        }
+        this.state = {}
     }
 
     render(){
-
-        const { storeRegistionDetail , address } = this.state
-
         return(
             <Container>
                 <Header style={{ backgroundColor: "#7A5032" }}>
                     <Left style={{ flex: 2 }} >
-                        <Icon name='arrow-back' style={{marginLeft:10,color:'white'}} onPress={ () => { createStore()}}/>
+                        <Icon name='arrow-back' style={{marginLeft:10,color:'white'}} onPress={ () => { goToProfile()}}/>
                     </Left>
                     <Body style={{ flex: 2.5 }}>
                         <Text style={{ color: "white" }}>ตั้งร้านเพิ่ม</Text>
                     </Body>
                     <Right style={{ flex: 1 }} />
                 </Header>
-                <Content>
-                    <Form>
-                        <ListItem itemDivider>
-                            <Text>ข้อมูลทั่วไปของร้าน</Text>
-                        </ListItem>
-                            <Field name = 'name' component={renderInput} />
-                            <Field name = 'phoneNumber' component={renderInput} />
-                            <Field name = 'description' component={renderInput} />
-                            <Field name = 'maxOfDeposit' component={renderInput} />
-                        <ListItem itemDivider>
-                            <Text>ฟอร์มแบบเก่า (without redux-form)</Text>
-                        </ListItem>
-                        <Item stackedLabel>
-                            <Label> ชื่อร้าน </Label>
-                            <Input onChangeText = { ( name )=> {this.setState({
-                                storeRegistionDetail : {
-                                    ...storeRegistionDetail,name
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel>
-                            <Label> เบอร์โทรศัพท์ </Label>
-                            <Input onChangeText = { ( phoneNumber )=> {this.setState({
-                                storeRegistionDetail : {
-                                    ...storeRegistionDetail,phoneNumber
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel>
-                            <Label> คำอธิบาย </Label>
-                            <Input onChangeText = { ( description )=> {this.setState({
-                                storeRegistionDetail : {
-                                    ...storeRegistionDetail,description
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel style={{marginBottom:5}}>
-                            <Label> จำนวนรับฝากสูงสุด </Label>
-                            <Input onChangeText = { ( maxOfDeposit )=> {this.setState({
-                                storeRegistionDetail : {
-                                    ...storeRegistionDetail,maxOfDeposit
-                                }
-                            })}} />
-                        </Item>
-                        <ListItem itemDivider>
-                            <Text>ตำแหน่งที่ตั้งของร้าน</Text>
-                        </ListItem>
-                        <Item stackedLabel> 
-                            <Label> ถนน </Label>
-                            <Input onChangeText = { ( street )=> {this.setState({
-                                address : {
-                                    ...address,street
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel> 
-                            <Label> เขต / อำเภอ </Label>
-                            <Input onChangeText = { ( district )=> {this.setState({
-                                address : {
-                                    ...address,district
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel> 
-                            <Label> จังหวัด </Label>
-                            <Input onChangeText = { ( province )=> {this.setState({
-                                address : {
-                                    ...address,province
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel> 
-                            <Label> รหัสไปรษณีย์ </Label>
-                            <Input onChangeText = { ( postcode )=> {this.setState({
-                                address : {
-                                    ...address,postcode
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel> 
-                            <Label> ละติจูด </Label>
-                            <Input onChangeText = { ( latitude )=> {this.setState({
-                                address : {
-                                    ...address,latitude
-                                }
-                            })}} />
-                        </Item>
-                        <Item stackedLabel>
-                            <Label> ลองจิจูด </Label>
-                            <Input onChangeText = { ( longitude )=> {this.setState({
-                                address : {
-                                    ...address,longitude
-                                }
-                            })}} />
-                        </Item>
-                    </Form>
-                </Content>
-                <Footer>
-                    <FooterTab style={{backgroundColor:'none'}}>
-                        <Button style={{backgroundColor:'green'}} full onPress={ ()=>{ createStore({...storeRegistionDetail,address}) }}> 
-                            <Text> ลงทะเบียนร้านรับฝาก </Text> 
-                        </Button>
-                    </FooterTab>
-                </Footer>
+                {storeForm(this.props)}
             </Container>
         )
     }
 }
 
 renderInput = ({input,label,type,meta:{ touched,error,warning}})=>{
-    let hashError = false;
-    if(error != undefined){
-        hashError = true
-    }
     switch(input.name){
-        case 'phoneNumber': 
+        case 'phoneNumber':
+        case 'maxOfDeposit':
+        case 'postcode' :
+        case 'latitude':
+        case 'longitude':
         return(
-            <Item stackedLabel error = {hashError}>
-                <Input keyboardType='number-pad' {...input}/>
-                {hashError ? <Text>{error}</Text>:<Text/>}
-            </Item>
-        )
-        case 'maxOfDeposit': 
-        return(
-            <Item stackedLabel error = {hashError}>
-                <Input keyboardType='number-pad' {...input}/>
-                {hashError ? <Text>{error}</Text>:<Text/>}
-            </Item>
+            <View style={{marginVertical:5 , paddingHorizontal:10}}>
+                <Text style={{marginHorizontal:15,marginVertical:2}}>{label} 
+                    <Text>{"     "}</Text>
+                    {touched && (error ? <Text note style={{color:'red'}}>{error}</Text>:<Text/>)}
+                </Text>
+                <Item bordered rounded>
+                    <Input keyboardType='number-pad' {...input}/>
+                </Item>
+            </View>
         )
         default : 
     }
     return(
-        <Item stackedLabel error = {hashError}>
-            <Input keyboardType='default' {...input}/>
-            {hashError ? <Text>{error}</Text>:<Text/>}
-        </Item>
+        <View style={{marginVertical:5 , paddingHorizontal:10}}>
+            <Text style={{marginHorizontal:15,marginVertical:2}}>{label}
+                <Text>{"     "}</Text>
+                {touched && (error ? <Text note style={{color:'red'}}>{error}</Text>:<Text/>)}
+            </Text>
+            <Item bordered rounded>
+                <Input keyboardType='default' {...input}/>
+            </Item>
+        </View>
+    )
+}
+
+const storeForm = props =>{
+    const {handleSubmit} = props
+    return(
+        <Content>
+            <Form>
+                <ListItem style={{backgroundColor:'#7A5032'}} itemDivider>
+                    <Text style={{color:'white'}}>ข้อมูลทั่วไปของร้าน</Text>
+                </ListItem>
+                    <Field name = 'name' component={renderInput} label='ชื่อร้าน' />
+                    <Field name = 'phoneNumber' component={renderInput} label='เบอร์ติดต่อร้าน' />
+                    <Field name = 'description' component={renderInput} label='คำอธิบายร้าน'/>
+                    <Field name = 'maxOfDeposit' component={renderInput} label='จำนวนรับฝากสูงสุด'/>
+                <ListItem style={{backgroundColor:'#7A5032'}} itemDivider>
+                    <Text style={{color:'white'}}>ตำแหน่งที่ตั้งของร้าน</Text>
+                </ListItem>
+                <Field name = 'street' component={renderInput} label='ถนน'/>
+                <Field name = 'district' component={renderInput} label='เขต / อำเภอ'/>
+                <Field name = 'province' component={renderInput} label='จังหวัด'/>
+                <Field name = 'postcode' component={renderInput} label='รหัสไปรษณีย์'/>
+                <Field name = 'latitude' component={renderInput} label='ละติจูด'/>
+                <Field name = 'longitude' component={renderInput} label='ลองจิจูด'/>
+            </Form>
+            <Footer>
+                <FooterTab style={{backgroundColor:'none'}}>
+                    <Button style={{backgroundColor:'green'}} full onPress={handleSubmit(submit)}> 
+                        <Text> ลงทะเบียนร้านรับฝาก </Text> 
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Content>
     )
 }
 
@@ -177,7 +102,10 @@ const validate = (value) =>{
     error.phoneNumber = '';
     error.description = '';
     error.maxOfDeposit = '';
-    let {name,phoneNumber,description,maxOfDeposit} = value
+    error.district = '';
+    error.province = '';
+    error.postcode = '';
+    let {name,phoneNumber,description,maxOfDeposit,street,district,province,postcode,latitude,longitude} = value
     if(name == undefined){
         error.name = 'กรุณาใส่ชื่อร้าน'
     }
@@ -185,16 +113,38 @@ const validate = (value) =>{
         error.phoneNumber = 'กรุณาใส่เบอร์ติดต่อร้าน'
     }
     if(description == undefined){
-        description = ''
+        description = '-'
     }
     if(maxOfDeposit == undefined){
-        error.maxOfDeposit = 'กรุณาระบุจำนวนที่รับฝากได้สูงสุด'
+        error.maxOfDeposit = 'กรุณาระบุจำนวน'
+    }
+    if(street == undefined){
+        error.description = 'กรุณาระบุรายละเอียด'
+    }
+    if(district == undefined){
+        error.district = 'กรุณาระบุเขต / อำเภอ'
+    }
+    if(province == undefined){
+        error.province = 'กรุณาระบุจังหวัด'
+    }
+    if(postcode == undefined || postcode.length != 5){
+        error.postcode = 'กรุณากรอกรหัส 5 หลัก'
+    }
+    if(latitude == undefined){
+        error.latitude = 'กรุณากรอกละติจูด'
+    }
+    if(longitude == undefined){
+        error.longitude = 'กรุณากรอกลองจิจูด'
     }
     return error
 }
 
-createStore = (storeDetail)=>{
-    axios.post('/store',storeDetail).then( response =>{
+submit = (value)=>{
+    let {name,phoneNumber,description,maxOfDeposit,street,district,province,postcode,latitude,longitude} = value
+    let storeDetail = {name,phoneNumber,description,maxOfDeposit,rating:0}
+    let address = {street,district,province,postcode,latitude,longitude}
+    let data = {...storeDetail,address}
+    axios.post('/store',data).then( response =>{
         alert(response.data)
     })
     goToProfile()
