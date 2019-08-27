ca68e17f47bfe8f27b3581a06789b70ed28fb21c
Vuttichai <vuttichai.lor@mail.kmutt.ac.th>
2019-08-27 16:53:45 +0700

    [FIX]body-parser

--- epet_frontend_customer/index.js
+++ epet_frontend_customer/index.js
@@ -12,5 +12,5 @@ import Config from "react-native-config";
 const API_URL = Config.API_URL;
 
 axios.defaults.baseURL = API_URL;
-axios.defaults.headers.common['Authorization'] = "Epet eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InRhbmFwYXQiLCJwYXNzd29yZCI6IiQyYSQxMCRxNFRoOHdBV0hKOWJFUmNMYW1NSjlPaUFCTHFyanJiTnBjVjFoN2ptNTVIdTJsZnNIMzR1aSIsImlhdCI6MTU2NTQzODk4NCwiZXhwIjoxNTY4MDMwOTg0fQ.igmzfzJWKAnTNl-pFtGiZz7jXWJFMztvh_3e78jRLhU";
+axios.defaults.headers.common['Authorization'] = "Epet eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InRhbmFwYXQiLCJwYXNzd29yZCI6IiQyYSQxMCRxNFRoOHdBV0hKOWJFUmNMYW1NSjlPaUFCTHFyanJiTnBjVjFoN2ptNTVIdTJsZnNIMzR1aSIsImlhdCI6MTU2Njg5ODcxMSwiZXhwIjoxNTY5NDkwNzExfQ.5q_s-ttb6RZB5R85aWuZai0vlhTgBh8qqStFCB76X2I";
 AppRegistry.registerComponent(appName, () => App);
