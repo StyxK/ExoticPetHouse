f7a38cc0bcef46cbd9034a476182f897dfa60f79
Vuttichai <vuttichai.lor@mail.kmutt.ac.th>
2019-09-23 13:49:38 +0700

    [ADD]persist

--- epet_frontend_customer/App.js
+++ epet_frontend_customer/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import Main from "./views/Main";
-import { createStore,applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk"
 import logger from "redux-logger"
-import {userReply} from './actions/ChatActions'
+import { userReply } from './actions/ChatActions'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store,persistor } from "./configStore"
 
-const store = createStore(rootReducer,applyMiddleware(thunk,logger));
+// const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 export default () => (
   <Provider store={store}>
-    <Main />
+    <PersistGate persistor={persistor}>
+      <Main />
+    </PersistGate>
   </Provider>
 );
---
+++ epet_frontend_customer/configStore.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import Logger from 'redux-logger'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import rootReducer from './reducers'
+import AsyncStorage from '@react-native-community/async-storage'
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    // blacklist : ['form','chat']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(
+    persistedReducer, applyMiddleware(thunk, Logger)
+)
+
+export const persistor = persistStore(store)
