f1fd1db680bd33cba06df9235dc04cabcc69a7f2
StyxK <nosomething050@gmail.com>
2019-10-22 03:01:36 +0700

    [UPDATE] chat in customer side

--- epet_frontend_customer/App.js
+++ epet_frontend_customer/App.js
@@ -5,9 +5,6 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk"
 import logger from "redux-logger"
-import { userReply } from './actions/ChatActions'
-import { PersistGate } from 'redux-persist/integration/react'
-// import { store,persistor } from "./configStore"
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 export default () => (
--- epet_frontend_customer/reducers/index.js
+++ epet_frontend_customer/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import petsReducer from "./petsReducer";
 import userReducer from "./userReducer";
-import chatReducer from "./ChatReducer";
 
 export default combineReducers({
   pets: petsReducer,
   user: userReducer,
-  chat: chatReducer
 });
