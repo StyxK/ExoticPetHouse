3d7ffa93029f1f3f162a303f920945186ad30391
StyxK <nosomething050@gmail.com>
2019-11-18 06:17:02 +0700

    [UPDATE] Customer ui

--- epet_frontend_customer/views/MyPet.js
+++ epet_frontend_customer/views/MyPet.js
@@ -6,66 +6,63 @@ import {
   Header,
   Body,
   Title,
-  Content,
   Card,
-  Grid,
-  Col,
-  Row,
-  DeckSwiper
+  View,
+  Label
 } from "native-base";
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import Config from "react-native-config";
+import { StyleSheet ,ImageBackground,TouchableHighlight} from "react-native";
 import { Actions } from "react-native-router-flux";
 import { connect } from "react-redux";
-import { addPet, setPets, updatePet, removePet } from "../actions";
-import PetCard from "../components/PetCard";
+import { addPet, updatePet, removePet } from "../actions";
 import NavFooter from "../components/NavFooter";
 import theme from "../theme";
-import { loading } from '../components/Loading'
+import Carousel,{Pagination} from 'react-native-snap-carousel'
 
 class MyPet extends Component {
 
   constructor(props){
     super(props)
-    this.state={
-      loading:true,
-      pets:[]
+    this.state = {
+      activeSlide : 0
     }
   }
 
-  componentDidMount(){
-    this.setState({
-      pets:this.renderGrid()
-    })
-  }
-
-  renderGrid = () => {
-    const {pets} = this.props
-    let list = []
-    console.log(pets)
-    for(let i=0;i <= parseInt(pets.length/3) ;i++){
-      list.push(
-        <Row style={{ height: 120 }}>
-          {pets.map((pet,index) => {
-            if(i*3 <= index && index < (i+1)*3){
-              return (
-                  <Col style={{ height: 115 ,width: 105,margin:7}}>
-                    <Card style={{flex:1,borderRadius:30,justifyContent:'center'}}>
-                      <CardItem button onPress={this.goToPetDescription(pet)} style={{borderRadius:30,justifyContent:'center'}}>
-                        <PetCard pet={pet}/>
-                      </CardItem>
-                    </Card>
-                  </Col>
-              )
-            }
-            }
-          )}
-        </Row>
-      )
-    }
-    this.setState({loading:false})
-    return list
+  renderItem = ({item}) => {
+    let image
+    item.image == null ?
+        image = require('../assets/no_image_available.jpeg')
+        :
+        image = {uri:item.image}
+    return (
+        <TouchableHighlight style={{flex:1,borderRadius:10}} underlayColor='rgba(0,0,0,0)' onPress={()=>this.goToPetDescription(item)}>
+          <Card style={{flex:5,borderRadius:10}}>
+                <ImageBackground
+                    source={image}
+                    imageStyle={{borderRadius:10}}
+                    style={{flex:1,resizeMode:'cover',borderRadius:10}}
+                >
+                    <CardItem style={{flex:0.2,backgroundColor:'rgba(0,0,0,0)',justifyContent:'flex-end'}}>
+                      {
+                        item.wasDeposit ? 
+                          <Label style={{color:theme.infoTextColor,fontSize:15,padding:5,borderRadius:10,backgroundColor:theme.successColor}}>อยู่ระหว่างการฝาก</Label>
+                          :
+                          <Label style={{color:theme.infoTextColor,fontSize:15,padding:5,borderRadius:10,backgroundColor:'grey'}}>ยังไม่ถูกฝาก</Label>
+                      }
+                    </CardItem>
+                    <CardItem style={{flex:1,backgroundColor:'rgba(0,0,0,0)'}}/>
+                    <CardItem style={{flex:0.25,borderBottomLeftRadius:10,borderBottomRightRadius:10,backgroundColor:'rgba(0,0,0,0.8)',flexDirection:'column'}}>
+                        <Title style={{fontSize:20,textAlign:'left'}}>
+                          {item.name}
+                        </Title>
+                        <Title style={{fontSize:10}}>
+                          {item.typeOfPet}
+                        </Title>
+                    </CardItem>
+                </ImageBackground>
+          </Card>
+        </TouchableHighlight>
+    )
   }
 
   render() {
@@ -77,17 +74,27 @@ class MyPet extends Component {
               <Title style={{ color: theme.primaryTextColor, fontSize: 20 }}>สัตว์เลี้ยงของฉัน</Title>
             </Body>
           </Header>
-          <Content padder style={{backgroundColor:theme.primaryColor}}>
-            {
-              this.state.loading ?
-                loading()
-              :
-                <Grid style={{justifyContent:'center',alignItems:'center'}}>
-                  {this.state.pets}
-                </Grid>
-            }
-          </Content>
-
+          <View padder style={{alignItems:'center',flex:1,backgroundColor:theme.primaryColor}}>
+            <Carousel
+              data={this.props.pets}
+              renderItem={this.renderItem}
+              sliderWidth={500}
+              itemWidth={300}
+              onSnapToItem={(index)=>{this.setState({activeSlide:index})}}
+            />
+            <Pagination
+              dotsLength={this.props.pets.length}
+              activeDotIndex={this.state.activeSlide}
+              dotStyle={{
+                  width: 10,
+                  height: 10,
+                  borderRadius: 5,
+                  backgroundColor: 'rgba(255, 255, 255, 0.92)'
+              }}
+              inactiveDotOpacity={0.4}
+              inactiveDotScale={0.6}
+            />
+          </View>
           <Fab
             style={{ backgroundColor: theme.secondaryColor }}
             position="bottomRight"
@@ -105,7 +112,7 @@ class MyPet extends Component {
     Actions.addPet({ addPet });
   };
 
-  goToPetDescription = pet => () => {
+  goToPetDescription = pet => {
     const { updatePet, removePet } = this.props;
     Actions.petDescription({ pet, updatePet, removePet });
   };
